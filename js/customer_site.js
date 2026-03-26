373885d70ae138703fddaea5d7522b17739caf56
AutoHorse <58280769+autohorse@users.noreply.github.com>
2026-03-26 14:30:11 +0800

    Add Aiqiyi API configuration to customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,10 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+  aiqiyi: {
+        api: 'https://iqiyizyapi.com/api.php/provide/vod',
+        name: 'TV-爱奇艺',
+    }
 };
 
 // 调用全局方法合并
